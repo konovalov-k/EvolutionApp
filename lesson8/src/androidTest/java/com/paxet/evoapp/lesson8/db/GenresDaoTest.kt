e2fd339e82a1632cf74141b23529f6c3f3397989
Konovalov Kirill <51828487+konovalov-k@users.noreply.github.com>
2021-04-04 22:37:39 +0300

    Add GenresDaoTest.kt

---
+++ lesson8/src/androidTest/java/com/paxet/evoapp/lesson8/db/GenresDaoTest.kt
@@ -0,0 +1,55 @@
+package com.paxet.evoapp.lesson8.db
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.filters.LargeTest
+import com.paxet.evoapp.lesson8.data.db.*
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+@LargeTest
+class GenresDaoTest {
+    private lateinit var appContext: Context
+    private val db by lazy { AppDatabase.getDBInstance(appContext) }
+    private lateinit var genresDao: GenresDao
+
+    private val movieId = "0"
+    private val genre = Genres(-99, "Test")
+    private val genres = listOf(
+        Genres(-100, "Test"),
+        Genres(-101, "Test"),
+        Genres(-102, "Test")
+    )
+
+    @Before
+    fun createDb() {
+        appContext = ApplicationProvider.getApplicationContext<Context>()
+        genresDao = db.genresDao
+    }
+
+    @Test
+    fun checkInsert() {
+        genresDao.run {
+            insert(genre)
+            Assert.assertTrue(getAll().contains(genre))
+
+            insert(genres)
+            Assert.assertTrue(getAll().containsAll(genres))
+        }
+    }
+
+    @Test
+    fun checkDelete(){
+        genresDao.run {
+            delete(genre)
+            Assert.assertFalse(getAll().contains(genre))
+
+            delete(genres)
+            Assert.assertFalse(getAll().containsAll(genres))
+
+            delete()
+            Assert.assertTrue(getAll().isEmpty())
+        }
+    }
+}
